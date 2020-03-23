2b908851249d2df8856089ab26986f569148c028
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-03-23 15:27:44 +0700

    cat eat rat

--- Cat.js
+++ Cat.js
@@ -9,7 +9,18 @@ let Cat = function (name, weight, speed) {
 
     this.chasing = function (rat) {
         if (this.speed > rat.speed){
-            console.log(this.name + " bắt được chuột " + rat.name)
+            console.log(this.name + " bắt được chuột " + rat.name);
+            rat.status = false;
+        }
+        else {
+            console.log(this.name + " không bắt được chuột " + rat.name)
+        }
+    }
+
+    this.eatRat = function (rat) {
+        if (!rat.status){
+            this.weight += rat.weight;
+            console.log("Chuot bị chén");
         }
     }
 }
